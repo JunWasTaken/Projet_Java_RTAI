bdc71fb681edc6c96063abc8d64d00d095ab9361
Jun <47633500+JunkieUnicorn@users.noreply.github.com>
2021-12-09 19:51:37 +0100

    javadoc finie

--- Application_8/src/controler/AmmoBox.java
+++ Application_8/src/controler/AmmoBox.java
@@ -29,24 +29,39 @@ public class AmmoBox extends Objet {
 	public Munitions[] getReload() {
 		return this.reload;
 	}
-//	@Override
-//	public void moving(int posX, int posY) {
-//		this.setPosX(this.getPosX()+posX);
-//		this.setPosY(this.getPosY()+posY);
-//	}
+	/**
+	 * Fonction de déplacement de l'objet. On utilise les coordonnées passées en paramètres pour remplacer celle de l'objet
+	 * @see Objet#setPosX(double)
+	 * @see Objet#setPosY(double)
+	 * @param posX
+	 * @param posY
+	 */
 	public void moving(double posX, double posY) {
 		this.setPosX(this.getPosX()+posX);
 		this.setPosY(this.getPosY()+posY);
 	}
+	/**
+	 * Fonction de déplacement de l'objet. Utilise l'attribut velocity de l'objet pour gérer le déplacement
+	 * @see Objet#setPosX(double)
+	 * @see Objet#setPosY(double)
+	 */
 	public void moving() {
 		this.setPosX(this.getPosX() + velocity.getX());
 		this.setPosY(this.getPosY() + velocity.getY());
 	}
+	/**
+	 * Fonction gérant l'impact de l'objet avec le joueur. on utilise la fonction setChargeur du vaisseau avec l'attribut reload de cette classe
+	 * @see Ship#setChargeur(Munitions[])
+	 * @param Ship
+	 */
 	@Override
 	public void onImpact(Ship joueur) {
 		joueur.setChargeur(this.reload);
 	}
 
+	/**
+	 * remplit le tableau reload de cette classe avec 10 objets de type Munitions
+	 */
 	private void createAmmoBox() {
 		this.reload = new Munitions[10];
 		for (int i=0; i<10; i++) {
